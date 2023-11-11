2cada3c8d95e5f3cf1b3786ce72c55a886605030
Stanislav <staska.golka@gmail.com>
2023-11-11 21:33:38 +0200

    added common elements

--- app/src/main/java/com/example/sigmaindustry/presentation/navgraph/NavGraph.kt
+++ app/src/main/java/com/example/sigmaindustry/presentation/navgraph/NavGraph.kt
@@ -1,18 +1,12 @@
 package com.example.sigmaindustry.presentation.navgraph
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
-import com.loc.newsapp.presentation.onboarding.OnBoardingViewModel
+import com.example.sigmaindustry.presentation.onboarding.OnBoardingViewModel
 
 
 @Composable

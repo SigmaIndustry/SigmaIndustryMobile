0bbe925f0ab558c960d1ca1b4734dbdc678eb060
nicourrrn <s57111702@gmail.com>
2023-11-11 21:03:46 +0200

    Add main activity

---
+++ app/src/main/java/com/example/sigmaindustry/presentation/mainActivity/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.loc.newsapp.presentation.mainActivity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
+import com.example.sigmaindustry.ui.theme.SigmaIndustryTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+            SigmaIndustryTheme(dynamicColor = false) {
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                Box(modifier = Modifier.background(MaterialTheme.colorScheme.background).fillMaxSize()) {
+                }
+            }
+        }
+    }
+}

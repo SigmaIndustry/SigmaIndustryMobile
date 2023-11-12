7999a07e5b89c26d7b76456a7e6770d2e06bfc92
nicourrrn <s57111702@gmail.com>
2023-11-13 00:56:42 +0200

    Fix run application, but content fron main activity not visable (( but theme changable...

--- app/src/main/java/com/example/sigmaindustry/SigmaIndastry.kt
+++ app/src/main/java/com/example/sigmaindustry/SigmaIndastry.kt
@@ -4,5 +4,6 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 
+// I can`t rename this class because ... stack (Manifest edit android:name not resolve)
 @HiltAndroidApp
-class MainActivity : Application()
+class MainActivity: Application()
--- app/src/main/java/com/example/sigmaindustry/presentation/mainActivity/MainActivity.kt
+++ app/src/main/java/com/example/sigmaindustry/presentation/mainActivity/MainActivity.kt
@@ -9,7 +9,10 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.core.view.WindowCompat
 import com.example.sigmaindustry.presentation.mainActivity.MainViewModel
 import com.example.sigmaindustry.ui.theme.SigmaIndustryTheme
@@ -22,10 +25,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        print("ping")
         setContent {
             SigmaIndustryTheme(dynamicColor = false) {
                 val isSystemInDarkMode = isSystemInDarkTheme()
                 Box(modifier = Modifier.background(MaterialTheme.colorScheme.background).fillMaxSize()) {
+                    Text("color", color = Color.White)
                 }
             }
         }

f6ecb6f915b09c846ea290f659685b2954d8ac05
Stanislav <staska.golka@gmail.com>
2023-11-17 17:26:03 +0200

    refactor

--- app/src/main/java/com/example/sigmaindustry/presentation/auth/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/sigmaindustry/presentation/auth/profile/ProfileScreen.kt
@@ -1,10 +1,7 @@
 package com.example.sigmaindustry.presentation.auth.profile
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import com.example.sigmaindustry.extension.observeLifecycle
 
 @Composable
 fun ProfileScreen(
@@ -12,15 +9,6 @@ fun ProfileScreen(
     state: ProfileScreenState,
     navigateToSelectAuth: () -> Unit
 ){
-    viewModel.observeLifecycle(LocalLifecycleOwner.current.lifecycle)
 
-    if(state.token != null) {
-        Column {
-
-            Text(text = state.token)
-        }
-    }
-    else{
-        navigateToSelectAuth.invoke()
-    }
+    Text(text = state.token ?: "null")
 }

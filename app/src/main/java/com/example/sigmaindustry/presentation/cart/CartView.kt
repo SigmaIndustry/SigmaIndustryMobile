bbf68b0ac9be86d31f5a8b412e26e1a4b996b6d6
Stanislav <staska.golka@gmail.com>
2023-12-18 16:37:25 +0200

    cosmetic fixes

--- app/src/main/java/com/example/sigmaindustry/presentation/cart/CartView.kt
+++ app/src/main/java/com/example/sigmaindustry/presentation/cart/CartView.kt
@@ -2,21 +2,17 @@ package com.example.sigmaindustry.presentation.cart
 
 import android.content.Intent
 import android.net.Uri
-import android.widget.Space
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -24,24 +20,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.sigmaindustry.data.remote.dto.EntriesResponse
 import com.example.sigmaindustry.presentation.Dimens
 import com.example.sigmaindustry.presentation.Dimens.ServiceCardSize
-import com.example.sigmaindustry.presentation.Dimens.ServiceImageHeight
-import kotlinx.coroutines.delay
 
 
 @Composable
@@ -86,14 +77,23 @@ fun CartCardView(entry: EntriesResponse) {
                 contentScale = ContentScale.Crop
             )
             Spacer(modifier = Modifier.width(Dimens.ExtraSmallPadding2))
-            Column {
-                Row {Text("Send to", style = TextStyle(fontWeight = FontWeight.Bold)); Text(entry.email)}
+            Column(horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center) {
+
+                    Text(
+                        "Service: ${entry.service.name}",
+                        style = TextStyle(fontWeight = FontWeight.Bold),
+                        fontSize = 20.sp
+                    )
+
                 val intent = remember { Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/maps/@${entry.service.geolocation.latitude},${entry.service.geolocation.longitude},18z")) }
                 Button(onClick = { context.startActivity(intent) }) {
-                    Text(text = "Open GoogleMaps")
+                    Text(text = "Open in GoogleMaps")
                 }
             }
         }
-        Row {Text("Message: ", style = TextStyle(fontWeight = FontWeight.Bold)); Text(entry.message.split("|")[0])}
+        Text("Sent to: ${entry.email}", style = TextStyle(fontWeight = FontWeight.Bold))
+        Spacer(modifier = Modifier.width(Dimens.ExtraSmallPadding2))
+        Row(verticalAlignment = Alignment.CenterVertically) {Text("Message: ", style = TextStyle(fontWeight = FontWeight.Bold)); Text(entry.message.split("|")[0])}
     }
 }
